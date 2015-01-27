22b4d24a3aa9f88b0d87f81fca2ef95f8d6d27ae
JacksonK <kontny1@gmail.com>
2015-01-26 20:53:26 -0600

    Final Updates - 2 and 3 are wrong
    These are the final updates to the problem.  While the sorting algorithm works
    just fine, I was unable to perfectly mimic the algorithm that Roughgarden used.
     For this reason, my algorithm is slightly off when counting the number of
     iterations.  The implementation is effective, with case 3 computing the output
     the fastest, but the answer will come up incorrect on the HW submission.

--- Homework2/quicksort.py
+++ Homework2/quicksort.py
@@ -2,34 +2,35 @@ import time
 
 def get_pivot(array, method):
     if method == 1:
-        pop_index = 0
+        pivot = array.pop(0)
     if method == 2:
-        pop_index = len(array) - 1
+        pivot = array.pop()
+        switch = array.pop(0)
+        array.append(switch)
     if method == 3:
         first = array[0]
         last = array[len(array) -1]
         mid_i = int(len(array)/2) + 1 if len(array) % 2 != 0 else len(array)/2
         middle = array[mid_i]
         if first < middle and first > last:
-            pop_index = 0
+            pivot = array.pop(0)
         elif middle > first and middle < last:
-            pop_index = mid_i
+            pivot = array.pop(mid_i)
+            switch = array.pop(0)
+            array.insert(switch, mid_i - 1)
         else:
-            pop_index = len(array) - 1
-    print array
-    pivot = int(array.pop(pop_index))
-    first_element = array.pop(0)
-    array.insert(pop_index, first_element)
-    print array
-    time.sleep(1)
+            pivot = array.pop()
+            switch = array.pop(0)
+            array.append(switch)
+
     return pivot, array
             
 def quick_sort(unsorted_array, method = 1):
-    sorted_array = []
+    sorted_array = [] 
     if len(unsorted_array) <= 1:
         return unsorted_array, 0
-    smaller_array = []
-    larger_array = []
+    smaller_array = [] 
+    larger_array = [] 
     comparisons = len(unsorted_array) - 1
     pivot_element, unsorted_array = get_pivot(unsorted_array, method) 
     for element in unsorted_array:
@@ -37,9 +38,10 @@ def quick_sort(unsorted_array, method = 1):
             larger_array.append(element)
         else:
             smaller_array.append(element)
-    a = len(smaller_array)
-    b = len(larger_array)
-    c = len(unsorted_array)
+            if len(larger_array) > 1:
+                larger_array.append(larger_array.pop(0))
+    if len(smaller_array) > 1:
+        smaller_array.insert(0, smaller_array.pop())
     sorted_smaller_array, comp_1 = quick_sort(smaller_array)
     sorted_larger_array, comp_2 = quick_sort(larger_array)
     for element in sorted_smaller_array:
@@ -48,16 +50,11 @@ def quick_sort(unsorted_array, method = 1):
     for element in sorted_larger_array:
         sorted_array.append(element)
     return sorted_array, comparisons + comp_1 + comp_2
-
 with open('IntegerList.txt') as f:
     large_array = []
     for number in f.readlines():
         large_array.append(int(number))
-    sorted_array, comparisons1 = quick_sort(large_array, 1)
-    sorted_array, comparisons2 = quick_sort(large_array, 2)
-    sorted_array, comparisons3 = quick_sort(large_array, 3)
-    print 'first method: ', comparisons1
-    print 'second method: ', comparisons2
-    print 'third method: ', comparisons3
-
-
+    # sorted_array, comparisons1 = quick_sort(large_array, 1)
+    sorted_array, comparisons = quick_sort(large_array, 2)
+    # sorted_array, comparisons = quick_sort(large_array, 3)
+    print comparisons
